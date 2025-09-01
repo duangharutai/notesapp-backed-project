8dd580f4b915060cf7af6bcd7048d2d415029707
duangharutai <duangharutaisaw@gmail.com>
2025-09-01 16:18:42 +0700

    Edit index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://notesapp-fronted-project.vercel.app",
+    "https://notesapp-fronted-project.vercel.app"
   ], // frontend domain
   credentials: true, // ✅ allow cookies to be sent
 };
